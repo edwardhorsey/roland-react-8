441913daefb4d3af0909807e89f6e412886ab985
Edward Horsey <edwardhorsey@me.com>
2020-07-20 18:28:56 +0100

    Divs on the steps

--- src/Components/ProgramSteps/ProgramSteps.jsx
+++ src/Components/ProgramSteps/ProgramSteps.jsx
@@ -5,7 +5,6 @@ import styles from './ProgramSteps.module.scss';
 class ProgramSteps extends Component {
 
   state = {
-    sequence: [ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0 ],
   }
 
   componentDidMount() {
@@ -14,22 +13,19 @@ class ProgramSteps extends Component {
   renderSteps = (num) => {
     let output = [];
     for (let i=0; i<num; i++) {
-      output.push(<Step step={i} key={i} logic={this.activateNote} group={(i+1)%4===0 ? true : false} />)
+      output.push(<Step step={i} key={i} logic={this.updateLoop} group={(i+1)%4===0 ? true : false} />)
     }
     return output;
   }
 
-  activateNote = (event) => {
-    let newArray = this.state.sequence;
-    newArray[Number(event.target.name)] = event.target.checked ? 1 : 0
-    this.setState({ sequence: newArray });
-    return this.props.updateLoop(this.props.title, newArray);
+  updateLoop = (num, state) => {
+    console.log(num, state)
+    this.props.updateLoop(num, state, this.props.title);
   }
 
   render() { 
 
     const steps =  this.renderSteps(16)
-    // const stepClass = this.props
 
     return (
       <article className={styles.instrument}>
--- src/Components/ProgramSteps/Step/Step.jsx
+++ src/Components/ProgramSteps/Step/Step.jsx
@@ -3,10 +3,22 @@ import styles from './Step.module.scss';
 
 
 class Step extends Component {
-  
+
+  state = {
+    selected: false,
+  }
+
+  sendToLoop = () => {
+    this.props.logic(this.props.step, !this.state.selected)
+    this.setState({ selected: !this.state.selected})
+  }
+
   render() {
-    const stepStyle = this.props.group ? styles.group : '';
-    return <input type="checkbox" className={stepStyle} name={this.props.step} onChange={this.props.logic} />;
+    // const stepStyle = this.props.group ? `${styles.step} ${styles.group}` : styles.step;
+    const stepStyle = this.state.selected ? styles.on : styles.off;
+    // return <input type="checkbox" className={stepStyle} name={this.props.step} onChange={this.props.logic} />;
+    return <div className={stepStyle} name={this.props.step} onClick={this.sendToLoop}></div>;
+
   }
 }
  
