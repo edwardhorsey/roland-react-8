b7d8ec2ac65baf188cebf9eee6fc45cc743164ac
Edward Horsey <edwardhorsey@me.com>
2020-07-22 21:11:59 +0100

    All logic on app now

--- src/engine/load-samples/load-samples.js
+++ src/engine/load-samples/load-samples.js
@@ -5,11 +5,20 @@ async function getFile(audioContext, filepath) {
   return audioBuffer;
 }
 
-export default async function setupSample(audioContext, files) {
+export async function setupSample(audioContext, files) {
     let obj = {};
     for (let prop in files) {
       const sample = await getFile(audioContext, files[prop])
       obj[prop] = sample
     }
+  return obj;
+}
+
+export function setupGainNodes(audiocontext, files) {
+  let obj = {};
+  for (let prop in files) {
+    const gain = audiocontext.createGain();
+    obj[prop] = gain;
+  }
   return obj;
 }
